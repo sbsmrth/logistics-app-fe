ec22d3f0ba192db9ab262149c5dca2c78151dedc
sbsmrth <andresf.mrh@gmail.com>
2025-06-03 17:38:51 -0500

    remove proxy

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://logistics-app-be-4.onrender.com", // URL del backend
-        changeOrigin: true, // Cambia el origen para evitar problemas de CORS
-      },
-    },
-  },
+  plugins: [react()]
 });
